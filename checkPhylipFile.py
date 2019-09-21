7a96b3bfac9a274b55cb116aa1f2676aacdef788
thulekm <55018346+thulekm@users.noreply.github.com>
2019-09-21 09:53:52 +0700

    Add files via upload

---
+++ checkPhylipFile.py
@@ -0,0 +1,77 @@
+﻿#convert phylip to fasta
+#Author: Thulek@gmail.com
+ 
+from os import listdir
+from os.path import isfile, join
+import os
+import sys
+import platform
+import subprocess
+import time
+import array as arr
+#import runCommand
+
+import argparse
+
+text = "===================\nCheck Phylip File and write the sequence's status to a log file\n===================="
+
+parser = argparse.ArgumentParser(description = text)  
+#parser.parse_args()  
+
+jobIds = arr.array('i')
+
+parser.add_argument("-f", "--filex", help="The phylip file")
+
+# read arguments from the command line
+args = parser.parse_args()
+
+filex = args.filex
+#output = args.output
+
+def countLines(files):
+	if(os.path.isfile(files)):
+		with open(files) as f:
+			return len(f.readlines())
+	else:
+		return 0
+
+
+if os.path.isfile(filex):
+	infile = open(filex,"r")
+	outfile = open(filex+".log","w")
+	i = 1
+	tax = 0
+	sites = 0
+	for line in infile:
+		if i > 1:
+			if len(line.strip()) >= 1:
+				if "\t" in line:
+					tx = line.split("\t",1)
+					outfile.write(""+tx[0].strip()+"\t")
+					outfile.write(str(len(tx[1].strip()))+"\t")
+					if(len(tx[1].strip()) == sites):
+						outfile.write("Ok\n")
+					else:
+						outfile.write("Nok\n")
+				elif " " in line:
+					tx = line.split(" ",1)
+					outfile.write(""+tx[0].strip()+"\t")
+					outfile.write(str(len(tx[1].strip()))+"\t")
+					if(len(tx[1].strip()) == sites):
+						outfile.write("Ok\n")
+					else:
+						outfile.write("Nok\n")
+		else:
+			tx = line.split(" ",1)
+			#tax = int(tx[0].strip())
+			sites = int(tx[1].strip())
+		i = i + 1
+	outfile.close()
+	infile.close()
+	if(countLines(filex+".log")>0):
+		print "Finish check file."
+	else:
+		print "Create log file error."
+else:
+	print "Input file's not exists."
+	
---
+++ correctPhylipFile.py
@@ -0,0 +1,86 @@
+﻿#Author: thulek@gmail.com
+#correct an error phylip file
+
+from os import listdir
+from os.path import isfile, join
+import os
+import sys
+import argparse
+
+text = 'correct an error phylip file'
+parser = argparse.ArgumentParser(description = text)  
+
+parser.add_argument("-f", "--filex", help="The Phylip")
+args = parser.parse_args()
+
+filex = args.filex
+
+filename = ""
+path = ""
+if "/" in filex:
+	pathex = filex.split("/")
+	filename = pathex[len(pathex)-1]
+	path = filex.strip(filename)
+else:
+	filename = filex
+
+def checkPar(filex):
+	vfile = open(filex,"r")
+	y = vfile.readline()
+	if "=" in y:
+		return True
+	else:
+		return False
+
+i = 1
+z = 1
+xx = 1
+if os.path.isfile(filex):
+	infile = open(filex,'r')
+	for line in infile:
+		if i == 1:
+			head_file = open(filex+"_head","w")
+			head_file.write(line.strip()+"\n")
+			head_file.close()
+		else:
+			if len(line.strip()) < 1:
+				z+=1
+			else:
+				temp_file = open(filex+"_"+str(z),"a+")
+				temp_file.write(line.strip()+"\n")
+				temp_file.close()
+		i+=1
+	if z >= 3:
+		t = 0
+		while xx < (z-1):
+			if(os.path.isfile(filex+"_"+str(xx+1)) and os.path.isfile(filex+"_1")):
+				if(checkPar(filex+"_"+str(xx+1))):
+					os.system("mv "+filex+"_"+str(xx+1)+" "+filex+"_"+str(z))
+				else:
+					os.system("python verticalJoin.py -f "+filex+"_1"+" -j "+filex+"_"+str(xx+1)+" -t 1")
+			else:
+				t+=1
+			xx+=1	
+		if t > 0:
+			if(os.path.isfile(filex+"_"+str(z))):
+				os.system("mv "+filex+"_"+str(z)+" "+filex+"_"+str(z-t))
+			z = z - t
+		if(checkPar(filex+"_"+str(z))): 
+			os.system("mv "+filex+"_"+str(z)+" "+path+"PAR_"+filename)
+		else:
+			os.system("python verticalJoin.py -f "+filex+"_1"+" -j "+filex+"_"+str(z)+" -t 1")
+		os.system("cat "+filex+"_head "+filex+"_1 > "+path+"output."+filename)
+		os.system("rm "+filex+"_*")
+	elif z==2:
+		if(checkPar(filex+"_"+str(z))): 
+			os.system("mv "+filex+"_"+str(z)+" "+path+"PAR_"+filename)
+		else:
+			os.system("python verticalJoin.py -f "+filex+"_1"+" -j "+filex+"_"+str(z)+" -t 1")
+		os.system("cat "+filex+"_head "+filex+"_1 > "+path+"output."+filename)
+		os.system("rm "+filex+"_*")
+	else:
+		#os.system("cat "+filex+"_head "+filex+"_1 > "+path+"output."+filename)
+		print("Don't need correct.")
+		os.system("rm "+filex+"_*")
+else:
+	print "File's not exists."
